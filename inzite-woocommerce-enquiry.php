499f639275213f54114799a113cd3a3195385f3c
Rasmus Jürs <rasmus@jurs.me>
2015-10-22 11:49:21 +0200

    Remove price and product sorter

--- inzite-woocommerce-enquiry.php
+++ inzite-woocommerce-enquiry.php
@@ -44,6 +44,8 @@ function alter_wc_default_templates() {
   remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+  remove_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 );
+  remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10);
 }
 
 // Add "Add to Enquiry" Button to sigle produkts
